a10f3a7071a44356f4b62906773a0cd5c1f833bc
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-02-13 23:48:35 +0200

    create footer

--- src/app/lib/component/Nav.tsx
+++ src/app/lib/component/Nav.tsx
@@ -1,5 +1,35 @@
+import Link from "next/link";
+
 const Nav = () => {
-  return <div className="w-11/12 mx-auto md:w-10/12 lg:w-9/12">Navbar</div>;
+  return (
+    <div className="w-11/12 mx-auto md:w-10/12 lg:w-9/12">
+      <nav className="flex flex-wrap items-center justify-between p-6 bg-white pb-9">
+        <Link href="/" className="text-5xl font-bold">
+          profile app.
+        </Link>
+        <ul className="flex gap-6">
+          <Link
+            href="/"
+            className="pt-3 text-gray-500 hover:text-blue-500 hover:underline hover:underline-offset-8"
+          >
+            HOME
+          </Link>
+          <Link
+            href="/about"
+            className="pt-3 text-gray-500  hover:text-blue-500 hover:underline hover:underline-offset-8"
+          >
+            ABOUT
+          </Link>
+          <Link
+            href="/login"
+            className="pt-3 text-gray-500  hover:text-blue-500 hover:underline hover:underline-offset-8"
+          >
+            LOGIN
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
