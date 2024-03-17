b5162bb606dd0f6cac8272a65acd4d1810dce45c
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-03-18 00:12:54 +0200

    fix production error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/lib/prisma";
-import * as bcrypt from "bcrypt";
-
-export const authOptions: AuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username",
-          type: "text",
-          placeholder: "Username",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Password",
-        },
-      },
-      async authorize(credentials) {
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials?.username,
-          },
-        });
-
-        if (!user) throw new Error("Username or password not correct");
-
-        if (!credentials?.password)
-          throw new Error("Please provide your password");
-        const isPasswordCorrect = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
-
-        if (!isPasswordCorrect)
-          throw new Error("Username or password not correct");
-
-        const { password, ...userWithoutPass } = user;
-        return userWithoutPass;
-      },
-    }),
-  ],
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./options";
 
 const handler = NextAuth(authOptions);
 
