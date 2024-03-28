a97c23401589346d793039877a94022bfd52e787
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-03-29 00:00:16 +0100

    update prisma schema and set up

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
