18022b986d25c18acf5209581cf0a14e582d4c29
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-04-05 00:21:04 +0200

    protect profile page from non logged in users

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,13 @@
-export default function Profile() {
+import { getSession } from "@/actions";
+import { redirect } from "next/navigation";
+
+export default async function Profile() {
+  const session = await getSession();
+
+  if (!session.isLoggedIn) {
+    redirect("/");
+  }
+
   return (
     <div className="w-11/12 mx-auto md:w-10/12 lg:w-9/12 flex items-center justify-center p-6">
       Your profile page. Only accesible for logged in users.
