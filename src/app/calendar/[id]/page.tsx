01b724e563580ec1254b61fe5e993c53e5899ce1
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-04-29 00:20:44 +0300

    remove FullCalendar

--- src/app/calendar/[id]/page.tsx
+++ src/app/calendar/[id]/page.tsx
@@ -1,18 +1,14 @@
 import { getSession, getUserData } from "@/actions";
 import { redirect } from "next/navigation";
-import Calendar from "@/app/components/Calendar";
-import { getEventData } from "@/lib/testFetchData";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const event = await getEventData();
   const id = params.id;
   const session = await getSession();
   const user = await getUserData(id);
-  console.log(event);
 
   if (!session.isLoggedIn) {
     redirect("/");
   }
 
-  return <Calendar />;
+  return <div>test</div>;
 }
